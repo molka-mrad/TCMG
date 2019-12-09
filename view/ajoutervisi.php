ae93cf33ceba65d8044cde75071cf8fcf3039488
aziz122 <47774685+aziz122@users.noreply.github.com>
2019-12-09 19:51:53 +0100

    Update ajoutervisi.php

--- view/ajoutervisi.php
+++ view/ajoutervisi.php
@@ -1,6 +1,8 @@
 <?PHP
-include "visiteur.php";
-include "visiteurC.php";
+
+include "../entities/visiteur.php";
+include "../core/visiteurC.php";
+
 
 if (isset($_POST['cin']) and isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['tel']) and isset($_POST['age'])and isset($_POST['inscription'])and isset($_POST['mdp'])and isset($_POST['email']) and isset($_POST['role'])){
 $visiteur1=new visiteurs($_POST['cin'],$_POST['nom'],$_POST['prenom'],$_POST['tel'], $_POST['age'], $_POST['inscription'], $_POST['mdp'], $_POST['email'],$_POST['role']);
