f27a044285a58b222f1d69c196ff0436d8145852
molka-mrad <47569082+molka-mrad@users.noreply.github.com>
2019-12-09 19:55:43 +0100

    Update boutique.php

--- view/boutique.php
+++ view/boutique.php
@@ -72,6 +72,7 @@
                   <div class="form-check">
                     <button class="btn btn-info" type="button" name="showpassword" id="showpassword" value="Show Password">Afficher Mot de Passe</button> 
                     <input type="submit" class="btn btn-info" name="button" id="button" ></input>
+                    <a href="recumdp.php"><input type="button"  value="MDP oublié?" class="btn btn-info"> </a>
                   </div>
               </form>
             <?php
