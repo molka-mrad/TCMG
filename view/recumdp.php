dadc87d0a766564ff8b38c85987cba19f24c114d
aziz122 <47774685+aziz122@users.noreply.github.com>
2019-12-13 01:55:54 +0100

    Update recumdp.php

--- view/recumdp.php
+++ view/recumdp.php
@@ -1,3 +1,14 @@
+
+<?php 
+include 'config.php';
+include '../entities/membre.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor\autoload.php';
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -48,31 +59,78 @@
                             <td class="nav-item"><a href="index.php" class="nav-link">Accueil</a></td>
                             <td class="nav-item"><a href="MonClub.php" class="nav-link">Notre Club</a></td>
                             <td class="nav-item"><a href="Inscription.php" class="nav-link">Inscription</a></td>
-                            <td class="nav-item"><a href="+/produits.php" class="nav-link">Notre Boutique</a></td>
+                            <td class="nav-item"><a href="boutique.php" class="nav-link">Notre Boutique</a></td>
                             <td class="nav-item"><a href="espacepriv.php" class="nav-link">Mon Espace</a></td>
+                            <td class="nav-item"><a href="Contact.php" class="nav-link">Contact</a></td>
                         </tr>
                     </table>
                 </div>
         </nav>
 
+                 <!-- ***************************** -->
+                 <?php
+ 
+     
+
+ if(isset($_POST['email']) and isset($_POST['va']))
+ {   
+ try
+     {
+         $bdd = new PDO('mysql:host=localhost;dbname=tcmg', 'root', '');
+     }
+     catch(Exception $e)
+     {
+         die('Erreur : '.$e->getMessage());
+     }
+ $sth = $bdd->prepare('SELECT user_pass,user_email FROM users WHERE user_email = :email');
+ $email = $_POST['email'];
+ $sth->bindParam(':user_pass', $mdp);
+ $sth->bindValue(':email', $email);
+//  $donnees = $sth->fetch();
+ if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ $destinataire = $email;
+ $sujet = "Mot de passe de votre compte" ;
+ $entete = "From: hammamiaziz100@hotmail.com" ;
+  
+ // Le lien d'activation est composé du login(log) et de la clé(cle)
+ $message = 'Bienvenue sur Tennis Club de Mégrine,
+  
+ Pour activer votre compte, ceci est votre mot de passe ' .$mdp. '
+ 
+ ---------------
+ Ceci est un mail automatique, Merci de ne pas y répondre.';
+  
+ ini_set('sendmail_from', 'hammamiaziz100@hotmail.com');
+ ini_set("SMTP","smtp.example.com" );
+  
+     
+ mail($destinataire, $sujet, $message, $entete) ; 
+
+  
+
+ }
+}
+ 
+ 
+  
+ 
+  
+  
+ ?>
+         <!-- *********************************** -->
+
         <div class="contenu" align="center">
         <br>
         <h2 style="color: #3d435c" > Recuperation du mot de passe</h2>
         <br>
-        <form   method="POST" >
+        <form   method="POST" action="recumdp.php">
           <h5 style="color: #3d435c" >email:</h5>
-                          <input type="email"  placeholder="votre adresse mail" name="recup_mail" class="btn btn-info" />
-                          <input type="submit" name="recup_submit" class="btn btn-info" value="Valider" />
+                          <input type="email"  placeholder="votre adresse mail" name="email" class="btn btn-info" />
+                          <input type="submit" name="va" class="btn btn-info" value="Valider" />
          </form>
-         <?php if(isset($error)){
-             echo '<span style="color:red">'.$error.'</span>';
-         }else
-         {
-             echo"</br>";
-         }
-         ?>
 
         </div>
         
+       
     </body>
 </html>
